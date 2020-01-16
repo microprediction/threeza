60b483e6d4b332a2272914217a57c5536a48fa35
pcotton <pcotton@Annes-MacBook-Pro-2.local>
2020-01-16 08:23:04 -0500

    version change

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="threeza",
-    version="0.0.1",
+    version="0.1.0",
     description="Fun fame and fortune",
     long_description=README,
     long_description_content_type="text/markdown",
