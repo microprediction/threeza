1031f5232734846da2a727ccd62ae5fbb5f19b6d
Peter Cotton <pcotton@Peters-Work-Laptop.local>
2020-01-19 19:53:12 -0500

    Still need to fix author_has_approved_pipe_page

---
+++ tests/test_mores.py
@@ -0,0 +1,3 @@
+from threeza.mores import author_has_approved_pipe_page
+
+assert author_has_approved_pipe_page(algo_name="threeza/Template",name="threeza-template-pipe-example-input.json")==True
--- threeza/mores.py
+++ threeza/mores.py
@@ -1,11 +1,10 @@
 
-from .web_utils import get_url
+from threeza.web_utils import get_url
 
 def author_has_approved_pipe_page(algo_name,name):
-	""" Does author approve of a page that invokes their algorithm ? """
-
-	assert "/" in algo_name, "Expecting full algorithm name including username (e.g.  threezatests/double)"
-	details_url = "https://algorithmia.com/webapi/algorithms/"+algo_name
+    """ Does author approve of a page that invokes their algorithm ? """
+    assert "/" in algo_name, "Expecting full algorithm name including username (e.g.  threezatests/double)"
+    details_url = "https://algorithmia.com/webapi/algorithms/"+algo_name
     try:
         details = json.loads(get_url(details_url))
     except Exception as e:
